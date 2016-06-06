9825bd77380a21768f14a1e73d6cdd8a54c8826f
cavanfulcher <cavanfulcher@gmail.com>
2016-06-06 19:01:39 -0400

    Add files via upload

---
+++ keller_project.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 07, 2016 at 12:58 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `keller_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bid`
+--
+
+CREATE TABLE IF NOT EXISTS `bid` (
+  `buyerEmailAddress` char(20) NOT NULL,
+  `bidDate` datetime NOT NULL,
+  `homeID` int(11) NOT NULL,
+  `bidAmount` decimal(7,2) NOT NULL,
+  PRIMARY KEY (`buyerEmailAddress`,`bidDate`,`homeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buyers`
+--
+
+CREATE TABLE IF NOT EXISTS `buyers` (
+  `buyer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) NOT NULL DEFAULT '',
+  `last_name` varchar(20) NOT NULL DEFAULT '',
+  `phone_num` varchar(10) NOT NULL DEFAULT '',
+  `address` varchar(40) NOT NULL DEFAULT '',
+  `city` varchar(20) NOT NULL DEFAULT '',
+  `state` varchar(20) NOT NULL DEFAULT '',
+  `zip` varchar(5) NOT NULL DEFAULT '',
+  `email` varchar(20) NOT NULL DEFAULT '',
+  PRIMARY KEY (`buyer_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `buyers`
+--
+
+INSERT INTO `buyers` (`buyer_id`, `first_name`, `last_name`, `phone_num`, `address`, `city`, `state`, `zip`, `email`) VALUES
+(2, 'John', 'Doe', '7065558888', '555 Broad Street', 'Augusta', 'Georgia', '30909', 'johndoe@email.com'),
+(3, 'Joe', 'Dirt', '8675309555', '123 Cool Street', 'Evans', 'Florida', '50412', 'joemullet@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `listing`
+--
+
+CREATE TABLE IF NOT EXISTS `listing` (
+  `listID` int(11) NOT NULL AUTO_INCREMENT,
+  `emailAddress` char(20) NOT NULL,
+  `homeID` int(11) NOT NULL,
+  `listDate` date NOT NULL,
+  `listPeriod` varchar(20) NOT NULL,
+  `periodDeadline` date NOT NULL,
+  PRIMARY KEY (`listID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
